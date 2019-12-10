9f325690064a2ed16b977e1c91815b1c1cc17c9d
evvyy <42327718+evvyy@users.noreply.github.com>
2019-12-09 23:40:23 -0800

    Update fine_tuning_VGG16_pt3.py

--- fine_tuning_VGG16_pt3.py
+++ fine_tuning_VGG16_pt3.py
@@ -1,7 +1,6 @@
 
-# coding: utf-8
 
-#Implementation of the tutorial:
+#Implementation of the tutorial (for learning purposes):
 #"Building powerful image classification models using very little data"
 #https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html
 
